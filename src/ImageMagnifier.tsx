6a32baf1579e221782d56ffc16eeb30d5a7079fe
Manjari Jain <manjari_jain@intuit.com>
2024-12-10 15:28:48 +0530

    success

--- src/ImageMagnifier.tsx
+++ src/ImageMagnifier.tsx
@@ -10,18 +10,44 @@ const ImageMagnifier: React.FC<IImageMagnifierProps> = ({ imageURL }) => {
   const [showMagnifier, setShowMagnifier] = useState(false);
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
 
+  const handleMouseHover = (e: any) => {
+    const { left, top, width, height } =
+      e.currentTarget.getBoundingClientRect();
+
+    const x = ((e.pageX - left) / width) * 100;
+    const y = ((e.pageY - top) / height) * 100;
+    setPosition({ x, y });
+    setCursorPosition({ x: e.pageX - left, y: e.pageY - top });
+  };
+
   return (
-    <div className="image-magnifier-container">
+    <div
+      className="image-magnifier-container"
+      onMouseEnter={() => setShowMagnifier(true)}
+      onMouseLeave={() => setShowMagnifier(false)}
+      onMouseMove={handleMouseHover}
+    >
       <img className="magnifier-img" src={imageURL} alt="working image" />
-      <div>
+      {showMagnifier && (
         <div
-          className="magnified-image"
           style={{
-            backgroundImage: `url(${imageURL})`,
-            backgroundPosition: `${position.x}% ${position.y}% `,
+            position: "absolute",
+            left: `${cursorPosition.x}px`,
+            top: `${cursorPosition.y}px`,
+            pointerEvents: "none",
+            transform: "scale(2)",
           }}
-        ></div>
-      </div>
+        >
+          <div
+            className="magnified-image"
+            style={{
+              backgroundImage: `url(${imageURL})`,
+              backgroundPosition: `${position.x}% ${position.y}% `,
+              zoom: 1,
+            }}
+          ></div>
+        </div>
+      )}
     </div>
   );
 };
